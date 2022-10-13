1433003957f23fdd953ff7455b7c3a291497453c
anaberia <ana.beria3@gmail.com>
2022-10-14 01:16:32 +0400

    map items list

--- src/List.js
+++ src/List.js
@@ -2,7 +2,18 @@ import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
 const List = ({ items }) => {
-  return <div className="grocery-list">grocery list</div>;
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default List;
