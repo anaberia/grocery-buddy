0a3b6971df3cb67c13580fd2296c5f69997c07cb
anaberia <ana.beria3@gmail.com>
2022-10-13 23:16:39 +0400

    add main html and handler

--- src/App.js
+++ src/App.js
@@ -9,7 +9,25 @@ function App() {
   const [editId, setEditId] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
-  return <h2>grocery project setup</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery buddy</h3>
+        <div className="form-control">
+          <input type="text" />
+          <button type="submit" className="submit-btn"></button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
